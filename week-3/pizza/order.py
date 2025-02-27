ddef1ecaa8cac010d21b47a2543a85ee13bd4e40
lookitsssonya <lookitsssonya@yandex.ru>
2025-02-27 14:22:21 +0300

    Добавлены задания 3 недели

---
+++ week-3/pizza/order.py
@@ -0,0 +1,41 @@
+import time
+
+class Order:
+    """Класс Заказ содержит информацию о заказе."""
+
+    order_counter = 0
+
+    def __init__(self):
+        """Конструктор класса."""
+        self.ordered_pizzas = []
+        Order.order_counter += 1
+        self.order_number = Order.order_counter
+
+    def __str__(self) -> str:
+        """Вернуть содержимое заказа и его сумму."""
+        res = f"Заказ №{self.order_number}\n"
+        for i, pizza in enumerate(self.ordered_pizzas, 1):
+            res += f"{i}. {pizza}\n"
+        res += f"Сумма заказа: {self.amount():.2f} р.\n"
+        return res
+
+    def add(self, pizza) -> None:
+        """Добавить пиццу в заказ."""
+        self.ordered_pizzas.append(pizza)
+        print(f"Пицца {pizza.name} добавлена!")
+
+    def amount(self) -> float:
+        """Вернуть сумму заказа."""
+        return sum(pizza.price for pizza in self.ordered_pizzas)
+
+    def complete(self) -> None:
+        """Выполнить заказ."""
+        print(f"Заказ поступил на выполнение...")
+        for i, pizza in enumerate(self.ordered_pizzas, 1):
+            print(f"{i}. {pizza.name}")
+            pizza.prepare()
+            pizza.bake()
+            pizza.slice()
+            pizza.pack()
+            time.sleep(1)
+        print(f"Заказ №{self.order_number} готов! Приятного аппетита!")
---
+++ week-3/pizza/pizza.py
@@ -0,0 +1,69 @@
+class Pizza:
+    """Класс Pizza содержит общие атрибуты для пиццы."""
+
+    def __init__(self):
+        """Конструктор класса."""
+        self.name = "Заготовка"
+        self.dough = "тонкое"
+        self.sauce = "кетчуп"
+        self.filling = []
+        self.price = 0
+
+    def __str__(self) -> str:
+        """Вернуть информацию о пицце."""
+        return (f"Пицца: {self.name} | Цена: {self.price:.2f} р.\n"
+                f"Тесто: {self.dough} Соус: {self.sauce}\n"
+                f"Начинка: {', '.join(self.filling)}")
+
+    def prepare(self) -> None:
+        """Сообщить о процессе подготовки."""
+        print(f"Начинаю готовить пиццу {self.name}")
+        print(f"  - замешиваю {self.dough} тесто...")
+        print(f"  - добавляю соус: {self.sauce}...")
+        print(f"  - и, конечно: {', '.join(self.filling)}...")
+
+    def bake(self) -> None:
+        """Сообщить о процессе запекания."""
+        print("Выпекаю пиццу... Готово!")
+
+    def slice(self) -> None:
+        """Сообщить о процессе нарезки."""
+        print("Нарезаю на аппетитные кусочки...")
+
+    def pack(self) -> None:
+        """Сообщить о процессе упаковки."""
+        print("Упаковываю в фирменную упаковку и готово!")
+
+
+class Peperoni(Pizza):
+    """Класс ПиццаПепперони дополняет класс Пицца."""
+
+    def __init__(self):
+        super().__init__()
+        self.name = "Пепперони"
+        self.sauce = "томатный"
+        self.filling = ["пепперони", "сыр моцарелла"]
+        self.price = 350.00
+
+
+class Barbecue(Pizza):
+    """Класс ПиццаБарбекю дополняет класс Пицца."""
+
+    def __init__(self):
+        super().__init__()
+        self.name = "Барбекю"
+        self.sauce = "барбекю"
+        self.filling = ["бекон", "ветчина", "зелень", "сыр моцарелла"]
+        self.price = 450.00
+
+
+class Seafood(Pizza):
+    """Класс ПиццаДарыМоря дополняет класс Пицца."""
+
+    def __init__(self):
+        super().__init__()
+        self.name = "Дары моря"
+        self.dough = "пышное"
+        self.sauce = "тар-тар"
+        self.filling = ["кальмары", "креветки", "мидии", "сыр моцарелла"]
+        self.price = 550.00
